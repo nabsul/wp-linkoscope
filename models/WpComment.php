7a8d1608d937e4b41f83cf13155311681e3f007c
Nabeel Sulieman <nabsul@outlook.com>
2015-09-11 18:22:33 -0700

    models for wordpress data

---
+++ models/WpComment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-09-11
+ * Time: 6:22 PM
+ */
+
+namespace app\models;
+
+
+class WpComment
+{
+
+}
---
+++ models/WpPost.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-09-11
+ * Time: 6:21 PM
+ */
+
+namespace app\models;
+
+
+class WpPost
+{
+
+}
