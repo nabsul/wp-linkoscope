bb583f6044b864a542f992734317fc04cc24b06a
Nabeel Sulieman <nabsul@outlook.com>
2015-09-14 09:55:19 -0700

    Add pretty URL support.

--- config/web.php
+++ config/web.php
@@ -7,6 +7,11 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'c3aFRv6MXhAW_1uetg6tPDAcn7j4yj_I',
