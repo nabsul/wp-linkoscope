cad6f32f82835c632d9223f130d8ea43fd249d53
Nabeel Sulieman <nabsul@outlook.com>
2015-09-15 07:56:47 -0700

    pruning some unnecessary code.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,8 +37,6 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             ['label' => 'Admin', 'url' => ['/admin']],
             Yii::$app->user->isGuest ?
                 ['label' => 'Login', 'url' => ['/site/login']] :
