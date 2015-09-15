f9378e5e56ea9c8f07d0bd3a10359f2ef1201c47
Nabeel Sulieman <nabsul@outlook.com>
2015-09-14 20:16:24 -0700

    switching client over to the yii's client

--- vendor/automattic/Rest/Org/OrgWpApi.php
+++ vendor/automattic/Rest/Org/OrgWpApi.php
@@ -8,9 +8,83 @@
 
 namespace automattic\Rest\Org;
 
+use automattic\Rest\iWpApi;
+use yii\authclient\OAuth1;
+use yii\authclient\OAuthToken;
 
-use Automattic\Rest\iWpApi;
+class OrgWpApi implements  iWpApi
+{
+    public $consumerKey = 'H0LzFuk95DvY';
+    public $consumerSecret = 'cnTCuCoiZyC9a2eZa3RHJrP0w550b1eDgruGLYnPcQXKNFyK';
+    public $blogUrl = 'http://localhost/auto';
+    public $requestUrl = 'http://localhost/auto/oauth1/request';
+    public $authorizeUrl = 'http://localhost/auto/oauth1/authorize';
+    public $accessUrl = 'http://localhost/auto/oauth1/access';
+    public $postUrl = 'http://localhost/auto/wp-json/wp/v2/posts';
+    public $typeUrl = 'http://localhost/auto/wp-json/wp/v2/types';
 
-class OrgWpApi implements  iWpApi {
+    public $token = null;
+    public $secret = null;
+    public $verifier = null;
 
+    /**
+     * @var Client
+     */
+    private $_client = null;
+
+    /**
+     * Gets an initial authorization token and returns the URL to go to get user consent
+     *
+     * @param $returnUrl string URL to return from after user authorization.
+     * @return string
+     */
+    public function authorize($returnUrl)
+    {
+        $oauthClient = $this->getAuthClient();
+        $requestToken = $oauthClient->fetchRequestToken(); // Get request token
+        $url = $oauthClient->buildAuthUrl($requestToken, ['oauth_callback' => $returnUrl]); // Get authorization URL
+        return $url; // Redirect to authorization URL
+    }
+
+    /**
+     * @param $token
+     * @param $verifier
+     * @return OAuthToken
+     * @throws \yii\base\Exception
+     */
+    public function access($token, $verifier)
+    {
+        $client = $this->getAuthClient();
+        return $client->fetchAccessToken(new OAuthToken(['token' => $token]), $verifier);
+    }
+
+    public function getLinks($tok)
+    {
+        return $this->get($this->postUrl, $tok);
+    }
+
+    public function getTypes($tok)
+    {
+        return $this->get($this->typeUrl, $tok);
+    }
+
+    private function get($url, $tok)
+    {
+        $oauthClient = $this->getAuthClient();
+        $oauthClient->accessToken = $tok;
+        $result = $oauthClient->api($url, 'GET', []);
+        return $result;
+    }
+
+    private function getAuthClient()
+    {
+        return new OAuth1([
+            'consumerKey' => $this->consumerKey,
+            'consumerSecret' => $this->consumerSecret,
+            'authUrl' => $this->authorizeUrl,
+            'requestTokenUrl' => $this->requestUrl,
+            'accessTokenUrl' => $this->accessUrl,
+        ]);
+
+    }
 }
