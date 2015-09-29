71db54db956f12050022649b5639d16e04d6ec11
Nabeel Sulieman <nabsul@outlook.com>
2015-09-28 20:35:01 -0700

    created class to integrate APIs into Yii

--- vendor/automattic/Yii/WpApi.php
+++ vendor/automattic/Yii/WpApi.php
@@ -9,6 +9,10 @@
 namespace automattic\Yii;
 
 use yii\base\Object;
+use Yii;
+use yii\helpers\FileHelper;
+use automattic\LinkoScope\OrgLinkoScope;
+use automattic\LinkoScope\ComLinkoScope;
 
 /**
  * Class YiiWpApi
@@ -18,7 +22,37 @@ class WpApi extends Object {
 	public $configPath = '@runtime';
 	public $configFileName = 'api.cfg';
 
-	public function getRestApi() {
+	public function getRestApi()
+	{
+		$files = FileHelper::findFiles(Yii::$app->runtimePath, [
+			'recursive' => false,
+			'only' => ['/api.cfg'],
+		]);
 
+		if (count($files) == 0)
+			return null;
+
+		$cfg = json_decode(file_get_contents($files[0]), true);
+		if (!Yii::$app->user->isGuest)
+		{
+			$cfg['accessToken'] = Yii::$app->user->identity->token;
+			$cfg['accessTokenSecret'] = Yii::$app->user->identity->secret;
+		}
+
+		switch($cfg['type'])
+		{
+			case 'org':
+				return new OrgLinkoScope($cfg);
+			case 'com':
+				return new ComLinkoScope($cfg);
+			default:
+				return null;
+		}
 	}
+
+    protected function saveConfig($api)
+    {
+        $file = Yii::$app->runtimePath . '/api.cfg';
+        file_put_contents($file, json_encode($api->getConfig()));
+    }
 }
