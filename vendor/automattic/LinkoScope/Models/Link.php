533a586f08458031d95bb8ab2990642b2df1c6a0
Nabeel Sulieman <nabsul@outlook.com>
2015-10-08 18:28:07 -0700

    Cleaning up Link model
    - added author name field
    - removed list of votes (not needed in current implementation)

--- vendor/automattic/LinkoScope/Models/Link.php
+++ vendor/automattic/LinkoScope/Models/Link.php
@@ -9,9 +9,10 @@ class Link extends Object
 	public $id;
     public $date;
 	public $authorId;
+	public $authorName;
 	public $title;
 	public $url;
 	public $votes;
-    public $voteList;
+	public $comments;
 	public $score;
 }
