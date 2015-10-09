c319fb6caeba407e0d520aad1b62f5bab5b004f5
Nabeel Sulieman <nabsul@outlook.com>
2015-10-08 17:02:15 -0700

    Use ListView to display links instead of GridView
    The display is more like the style of Hacker News.
    Table was not very pretty.

---
+++ views/link/_linkItem.php
@@ -0,0 +1,22 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $model automattic\LinkoScope\models\Link */
+?>
+
+<div>
+    <div>
+        <?= ($index + 1) ?>:
+        [<?= Html::a($model->title, $model->url, ['target' => '_blank',]); ?>]
+        (<?= parse_url($model->url, PHP_URL_HOST) ?>)
+    </div>
+    <div style="top: 0px; float: right;">
+        <?= Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view', 'id' => $model->id,], ['title' => 'View']) ?>
+        <?= Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', ['up', 'id' => $model->id,], ['title' => 'Up']) ?>
+        <?= Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', ['down', 'id' => $model->id,], ['title' => 'Down']) ?>
+        <?= Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete', 'id' => $model->id,], ['title' => 'Delete']) ?>
+    </div>
+</div>
+<div>
+    <?= "$model->authorName | $model->date | $model->votes votes | $model->comments comments" ?>
+</div>
--- views/link/index.php
+++ views/link/index.php
@@ -1,55 +1,17 @@
 <?php
 
-use yii\grid\GridView;
-use yii\grid\ActionColumn;
 use yii\helpers\Html;
-use yii\helpers\Url;
-use automattic\LinkoScope\Models\Link;
+use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
 
 ?>
 <h1>Links Index</h1>
 
-<?= GridView::widget([
+<?= ListView::widget([
     'dataProvider' => $data,
-    'columns' => [
-        'id',
-        'authorId',
-        'date',
-        'votes',
-        'voteList' => ['label' => 'VoteList', 'value' => function(Link $link){return json_encode($link->voteList);}],
-        'score',
-        'title',
-        'url',
-        'actions' => [
-            'class' => ActionColumn::className(),
-            'template' => '{view} {delete} {up} {down}',
-            'urlCreator' => function($c, $m, $k, $i){
-                return Url::to([$c, 'id' => $m->id]);
-            },
-            'buttons' => [
-                'up' => function ($url, $model, $key) {
-                    $options = array_merge([
-                        'title' => Yii::t('yii', 'Up'),
-                        'aria-label' => Yii::t('yii', 'Up'),
-                        'data-pjax' => '0',
-                    ]);
-                    return Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', $url, $options);
-                },
-                'down' => function ($url, $model, $key) {
-                    $options = array_merge([
-                        'title' => Yii::t('yii', 'Dn'),
-                        'aria-label' => Yii::t('yii', 'Dn'),
-                        'data-pjax' => '0',
-                    ]);
-                    return Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', $url, $options);
-                },
-            ],
-        ],
-    ]
+    'itemView' => '_linkItem',
+    'options' => ['class' => 'striped'],
 ]); ?>
 
 <?= Html::a('Add New', ['new'], ['class' => 'btn btn-default btn-success']); ?>
-
-
