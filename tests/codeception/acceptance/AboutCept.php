be019e8178fd7fbb946b111d028e03551512c0e6
Nabeel Sulieman <nabsul@outlook.com>
2015-10-14 20:33:52 -0700

    Update Home and About acceptance tests
    Updated based on what's on those pages.

--- tests/codeception/acceptance/AboutCept.php
+++ tests/codeception/acceptance/AboutCept.php
@@ -5,6 +5,12 @@ use tests\codeception\_pages\AboutPage;
 /* @var $scenario Codeception\Scenario */
 
 $I = new AcceptanceTester($scenario);
+$I->amOnPage('/');
 $I->wantTo('ensure that about works');
-AboutPage::openBy($I);
-$I->see('About', 'h1');
+$I->click('About');
+$I->see('LinkoScope', 'h1');
+$I->see('WebApp');
+$I->see('WordPress.com API', 'a');
+$I->see('plugin API', 'a');
+$I->see('Hacker News', 'a');
+$I->see('scoring');
