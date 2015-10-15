a32bd1d8c70eb3cb8cacaa01e0929e430880ee90
Nabeel Sulieman <nabsul@outlook.com>
2015-10-14 20:27:55 -0700

    Link view hides action buttons when user isn't logged in.

--- views/link/_linkItem.php
+++ views/link/_linkItem.php
@@ -10,13 +10,15 @@ use yii\helpers\Html;
         [<?= Html::a($model->title, $model->url, ['target' => '_blank',]); ?>]
         (<?= parse_url($model->url, PHP_URL_HOST) ?>)
     </div>
+<?php if (!Yii::$app->user->isGuest) : ?>
     <div style="top: 0px; float: right;">
         <?= Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view', 'id' => $model->id,], ['title' => 'View']) ?>
         <?= Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', ['up', 'id' => $model->id,], ['title' => 'Up']) ?>
         <?= Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', ['down', 'id' => $model->id,], ['title' => 'Down']) ?>
         <?= Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete', 'id' => $model->id,], ['title' => 'Delete']) ?>
     </div>
+<?php endif; ?>
 </div>
 <div>
-    <?= "$model->authorName ($model->authorId) | $model->date | $model->votes votes | $model->comments comments" ?>
+    <?= "$model->authorName ($model->authorId) | $model->date | $model->votes votes | $model->comments comments | $model->score points" ?>
 </div>
