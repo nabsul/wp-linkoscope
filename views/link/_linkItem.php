5aaacf9e3313f63cd3ed9adc1710d03930ceb64e
Nabeel Sulieman <nabsul@outlook.com>
2015-10-15 18:09:58 -0700

    Move up/down votes from the far right to just before the title
    Now that it's only two icons (and will be only 1 soon), it makes more sense to put it here.

--- views/link/_linkItem.php
+++ views/link/_linkItem.php
@@ -7,13 +7,13 @@ use yii\helpers\Html;
 <div>
     <div>
         <?= ($index + 1) ?>:
+        <?= Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', ['up', 'id' => $model->id,], ['title' => 'Up']) ?>
+        <?= Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', ['down', 'id' => $model->id,], ['title' => 'Down']) ?>
         [<?= Html::a($model->title, $model->url, ['target' => '_blank',]); ?>]
         (<?= parse_url($model->url, PHP_URL_HOST) ?>)
     </div>
 <?php if (!Yii::$app->user->isGuest) : ?>
     <div style="top: 0px; float: right;">
-        <?= Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', ['up', 'id' => $model->id,], ['title' => 'Up']) ?>
-        <?= Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', ['down', 'id' => $model->id,], ['title' => 'Down']) ?>
         <?= '' /* Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete', 'id' => $model->id,], ['title' => 'Delete']) */ ?>
     </div>
 <?php endif; ?>
