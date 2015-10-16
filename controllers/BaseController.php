145077c75acfbce4f18edc0c776b25d85e802b35
Nabeel Sulieman <nabsul@outlook.com>
2015-10-15 18:19:22 -0700

    Implementation is now in the LinkoScope component. Using it.

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -2,44 +2,22 @@
 
 namespace app\controllers;
 
-use ShortCirquit\LinkoScopeApi\ComLinkoScope;
-use ShortCirquit\LinkoScopeApi\OrgLinkoScope;
+use ShortCirquit\LinkoScopeApi\iLinkoScope;
 use Yii;
 use yii\web\Controller;
-use yii\helpers\FileHelper;
 
 class BaseController extends Controller
 {
     /**
-     * @return ComLinkoScope|OrgLinkoScope|null
+     * @return iLinkoScope
      */
     protected function getApi()
 	{
-		$files = FileHelper::findFiles(Yii::$app->runtimePath, [
-			'recursive' => false,
-			'only' => ['/api.cfg'],
-		]);
-
-		if (count($files) == 0)
-			return null;
-
-		$cfg = json_decode(file_get_contents($files[0]), true);
-		if (!Yii::$app->user->isGuest)
-		{
-			$cfg['token'] = Yii::$app->user->identity->token;
-			$cfg['tokenSecret'] = Yii::$app->user->identity->secret;
-		}
-
-		$classNAme = $cfg['type'];
-        $api = new $classNAme($cfg);
-        return $api;
+		return Yii::$app->linko->getApi();
 	}
 
 	protected function saveConfig($api)
 	{
-		$file = Yii::$app->runtimePath . '/api.cfg';
-		$cfg = $api->getConfig();
-        $cfg['type'] = get_class($api);
-		file_put_contents($file, json_encode($cfg, JSON_PRETTY_PRINT));
+		Yii::$app->linko->saveConfig($api);
 	}
 }
