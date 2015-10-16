32db8f3cc3f4b0681e9f13cc5c4a7881498d5d16
Nabeel Sulieman <nabsul@outlook.com>
2015-10-15 18:14:38 -0700

    Create component to handle LinkoScope api object initialization
    This is a cleaner way to automatically load the api object in the Yii App.
    It will be especially useful when the component is used in both the WebApp and the ConsoleApp.

---
+++ components/LinkoScope.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-15
+ * Time: 3:55 PM
+ */
+
+namespace app\components;
+
+
+use ShortCirquit\LinkoScopeApi\iLinkoScope;
+use yii\base\Component;
+use app\models\User;
+use Yii;
+
+class LinkoScope extends Component
+{
+    public $apiConfigFile;
+
+    private $api = null;
+
+    public function __construct($cfg = []){
+        parent::__construct($cfg);
+    }
+
+    /**
+     * @return iLinkoScope
+     */
+    public function getApi()
+    {
+        if ($this->api === null)
+            $this->readConfig();
+
+        return $this->api;
+    }
+
+    private function readConfig(){
+        if (!file_exists($this->apiConfigFile))
+            return null;
+
+        $cfg = json_decode(file_get_contents($this->apiConfigFile), true);
+
+        if (!Yii::$app->user->isGuest) {
+            /** @var User $id */
+            $id = Yii::$app->user->identity;
+            $cfg['token'] = $id->token;
+            $cfg['tokenSecret'] = $id->secret;
+        }
+
+        $className = $cfg['type'];
+        $this->api = new $className($cfg);
+        return $this->api;
+    }
+
+    public function saveConfig(iLinkoScope $api)
+    {
+        $file = $this->apiConfigFile;
+        $cfg = $api->getConfig();
+        $cfg['type'] = get_class($api);
+        file_put_contents($file, json_encode($cfg, JSON_PRETTY_PRINT));
+    }
+}
