14c59fffa5a2c4e159e58e53279193c9d70e01f8
Nabeel Sulieman <nabsul@outlook.com>
2015-10-15 17:55:39 -0700

    Move "Add Link" button to top of page
    It will be annoying at the bottom when there are a lot of links.
    This closes #10

--- views/link/index.php
+++ views/link/index.php
@@ -6,14 +6,12 @@ use yii\widgets\ListView;
 /* @var $this yii\web\View */
 
 ?>
-<h1>Links Index</h1>
-
+<h3>Shared Links:</h3>
+<?php if (!Yii::$app->user->isGuest) : ?>
+    <?= Html::a('Add New', ['new'], ['class' => 'btn btn-default btn-success']); ?>
+<?php endif; ?>
 <?= ListView::widget([
     'dataProvider' => $data,
     'itemView' => '_linkItem',
     'options' => ['class' => 'striped'],
 ]); ?>
-
-<?php if (!Yii::$app->user->isGuest) : ?>
-<?= Html::a('Add New', ['new'], ['class' => 'btn btn-default btn-success']); ?>
-<?php endif; ?>
