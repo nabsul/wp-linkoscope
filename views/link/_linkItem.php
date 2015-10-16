503e03c03c1aa93748a6eda6a2bc502d736027f5
Nabeel Sulieman <nabsul@outlook.com>
2015-10-15 18:08:12 -0700

    Turn the display of the number of comments into a link to the comments.
    Link HN, it will also just show "discuss" when there are no comments yet.
    Also delete the view (eye shaped) icon since it's redundant and not as clear.
    
    closes #5

--- views/link/_linkItem.php
+++ views/link/_linkItem.php
@@ -12,7 +12,6 @@ use yii\helpers\Html;
     </div>
 <?php if (!Yii::$app->user->isGuest) : ?>
     <div style="top: 0px; float: right;">
-        <?= Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['view', 'id' => $model->id,], ['title' => 'View']) ?>
         <?= Html::a('<span class="glyphicon glyphicon-arrow-up"></span>', ['up', 'id' => $model->id,], ['title' => 'Up']) ?>
         <?= Html::a('<span class="glyphicon glyphicon-arrow-down"></span>', ['down', 'id' => $model->id,], ['title' => 'Down']) ?>
         <?= '' /* Html::a('<span class="glyphicon glyphicon-trash"></span>', ['delete', 'id' => $model->id,], ['title' => 'Delete']) */ ?>
@@ -20,5 +19,10 @@ use yii\helpers\Html;
 <?php endif; ?>
 </div>
 <div>
-    <?= "$model->authorName | ". date(DATE_RFC2822, strtotime($model->date)) . " | $model->votes votes | $model->comments comments" ?>
+    <?=
+        "$model->authorName | " .
+        date(DATE_RFC2822, strtotime($model->date)) .
+        " | $model->votes votes | " .
+        Html::a($model->comments == 0 ? "discuss" : "$model->comments comments", ['link/view', 'id' => $model->id])
+    ?>
 </div>
