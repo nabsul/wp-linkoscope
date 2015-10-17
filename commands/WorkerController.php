34c2739c22e78375f9527422dc8cf89ededcfa32
Nabeel Sulieman <nabsul@outlook.com>
2015-10-16 17:30:53 -0700

    Adding console comment to run worker job
    To run the service: ./yii worker/run

--- commands/WorkerController.php
+++ commands/WorkerController.php
@@ -10,13 +10,19 @@ namespace app\commands;
 
 use app\models\Job;
 use yii\console\Controller;
+use Yii;
 
 class WorkerController extends Controller
 {
     public function actionStatus()
     {
-        echo "Unexecuted jobs: " . Job::find()->where('status IS NULL')->count() . "\n";
-        echo "Running    jobs: " . Job::find()->where('status = "running"')->count() . "\n";
-        echo "Completed  jobs: " . Job::find()->where('status = "completed"')->count() . "\n";
+        echo "Pending   jobs: " . Job::find()->where('status IS NULL')->count() . "\n";
+        echo "Running   jobs: " . Job::find()->where('status = "running"')->count() . "\n";
+        echo "Completed jobs: " . Job::find()->where('status = "completed"')->count() . "\n";
+    }
+
+    public function actionRun()
+    {
+        Yii::$app->worker->Run();
     }
 }
