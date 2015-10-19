32ef17c1a1bd8d6332e5a49a332363ec25037c85
Nabeel Sulieman <nabsul@outlook.com>
2015-10-19 11:20:45 -0700

    Create initial Crawler component
    This component will be used to extract the title from a URL submitted by the user.

---
+++ components/Crawler.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nabeel
+ * Date: 2015-10-16
+ * Time: 6:42 PM
+ */
+
+namespace app\components;
+
+
+use yii\base\Component;
+
+class Crawler extends Component
+{
+
+    public function readTitle($url)
+    {
+        $content = $this->fetchUrl($url);
+        $crawler = new \Symfony\Component\DomCrawler\Crawler();
+        $crawler->addHtmlContent($content);
+        $node = $crawler->filterXPath('html/head/title');
+        if ($node->count() > 0)
+            return $node->first()->text();
+        return null;
+    }
+
+    private function fetchUrl($url){
+        $curlOptions = [
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+        ];
+        $curl = curl_init($url);
+
+        foreach ($curlOptions as $option => $value) {
+            curl_setopt($curl, $option, $value);
+        }
+
+        $response = curl_exec($curl);
+        return $response;
+    }
+}
