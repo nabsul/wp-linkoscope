3f58f4931bf090448f3c168a8a35b4cab4be630d
Nabeel Sulieman <nabsul@outlook.com>
2015-10-19 20:57:51 -0700

    Updated Home page acceptance test to latest UI changes.

--- tests/codeception/acceptance/HomeCept.php
+++ tests/codeception/acceptance/HomeCept.php
@@ -4,8 +4,8 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
-$I->amOnPage(Yii::$app->homeUrl);
+$I->amOnPage('/');
 $I->see('LinkoScope');
 $I->seeInTitle('LinkoScope');
 $I->seeLink('Login');
-$I->see('Links Index');
+$I->see('Shared Links:');
