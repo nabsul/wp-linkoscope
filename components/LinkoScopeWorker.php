49a9ca6eab60bed359a4dd4873e8456faddd49c5
Nabeel Sulieman <nabsul@outlook.com>
2015-10-20 21:00:50 -0700

    Debugging the LinkoScopeWorker
    - Add more verbose debugging statements
    - Update to use getConsoleApi from LinkoScope component

--- components/LinkoScopeWorker.php
+++ components/LinkoScopeWorker.php
@@ -11,20 +11,26 @@ namespace app\components;
 use yii\base\Component;
 use Yii;
 use app\models\Job;
+use yii\log\Logger;
 
 class LinkoScopeWorker extends Component
 {
 
     public function Run()
     {
+        $count = 0;
         while($this->keepRunning())
         {
             $job = Job::find()->where('status IS NULL')->one();
             if ($job === null){
+                if ($count % 10 == 0)
+                    echo "sleeping at " . time() . "\n";
+                $count++;
                 sleep(1);
                 continue;
             }
 
+            echo "Got Job: job->id\n";
             if (!$this->assignJob($job))
                 continue;
 
@@ -45,11 +51,15 @@ class LinkoScopeWorker extends Component
 
     private function runJob(Job $job)
     {
+        echo "Running $job->id $job->type $job->arguments\n";
         try
         {
             switch($job->type){
-                case 'update_link':
+                case 'refresh_link':
                     $this->updateLink($job);
+                    break;
+                case 'refresh_comment':
+                    $this->updateComment($job);
                     break;
                 default:
                     $this->failJob($job, "unknown job type: $job->type");
@@ -58,25 +68,32 @@ class LinkoScopeWorker extends Component
         }
         catch (\Exception $e)
         {
+            echo "Job failed with exception: " . $e->getMessage() . "\n";
             $this->failJob($job, $e->getMessage());
         }
+
+        echo "done\n";
+        $job->status = 'complete';
+        $job->save();
     }
 
     private function updateLink(Job $job)
     {
-        $api = $this->getLinkoScope()->getApi();
+        $api = $this->getLinkoScope()->getConsoleApi();
         $link = $api->getLink($job->args->id);
-        $api->updateLink($link);
+        echo "Sore was $link->score \n";
+        $link = $api->updateLink($link);
+        echo "Sore is  $link->score \n";
     }
 
     private function updateComment(Job $job)
     {
-        $api = $this->getLinkoScope()->getApi();
+        $api = $this->getLinkoScope()->getConsoleApi();
         $link = $api->getComment($job->args->id);
         $api->updateComment($link);
     }
 
-    private function failJob($job, $msg)
+    private function failJob(Job $job, $msg)
     {
         Yii::getLogger()->log("Job with ID $job->id failed: " . $msg, Logger::LEVEL_ERROR);
         $job->status = 'failed';
