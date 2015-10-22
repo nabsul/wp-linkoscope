71432137cb6c3e90afcd1df4f53c695d2ce49142
Nabeel Sulieman <nabsul@outlook.com>
2015-10-22 09:23:20 -0700

    Adding two new commands to the console utility
    - Add links from HackerNews for testing purposes
    - List all users on the blog

--- commands/UtilController.php
+++ commands/UtilController.php
@@ -9,29 +9,65 @@
 namespace app\commands;
 
 
-use ShortCirquit\LinkoScopeApi\OrgLinkoScope;
+use Httpful\Request;
+use ShortCirquit\LinkoScopeApi\Models\Link;
+use ShortCirquit\LinkoScopeApi\iLinkoScope;
 use yii\console\Controller;
 
 class UtilController extends Controller
 {
-
     public function actionClearAll(){
         echo "WARNING! This will delete EVERYTHING from your blog!\n";
         $input = readline("If you're sure, type yeS (yes, with a capital S): ");
         if ($input !== 'yeS')
             return;
 
-        $config = json_decode(file_get_contents(\Yii::$app->runtimePath . '/api.cfg'), true);
-        $config['token'] = $config['adminToken'];
-        if (isset($config['adminSecret']))
-            $config['tokenSecret'] = $config['adminSecret'];
-        $api = new $config['type']($config);
+        /** @var iLinkoScope $api */
+        $api = \Yii::$app->linko->getConsoleApi();
 
-        while(count($posts = $api->getLinks()) > 0){
+        while(count($posts = $api->getLinks()->links) > 0){
             foreach ($posts as $post){
                 echo "Deleting {$post->id}: {$post->title}\n";
                 $api->deleteLink($post->id);
             }
+        }
+    }
+
+    public function actionAddNews($count = 20)
+    {
+        /** @var iLinkoScope $api */
+        $api = \Yii::$app->linko->getConsoleApi();
+        $stories = Request::get('https://hacker-news.firebaseio.com/v0/newstories.json')->send()->body;
+        $users = $api->getAccounts();
+        $uCount = count($users);
+
+        foreach ($stories as $id)
+        {
+            $story = Request::get("https://hacker-news.firebaseio.com/v0/item/$id.json")->send()->body;
+            if (!isset($story->title, $story->url))
+                continue;
+            echo json_encode($story) . "\n";
+            $link = new Link([
+                'title' => $story->title,
+                'url' => $story->url,
+                'authorId' => $users[$count % $uCount]->id
+            ]);
+            echo json_encode($link) . "\n";
+            $api->addLink($link);
+            echo "Saved {$story->title}: {$story->url}\n";
+
+            if (--$count <= 0)
+                break;
+        }
+    }
+
+    public function actionAccounts()
+    {
+        /** @var iLinkoScope $api */
+        $api = \Yii::$app->linko->getConsoleApi();
+        foreach ($api->getAccounts() as $u)
+        {
+            echo json_encode($u) . "\n";
         }
     }
 }
